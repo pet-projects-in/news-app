f72a2c94d4e5d197fd66ea6079958c29a4525d72
Nishant Ranjan <nishantranjan78@gmail.com>
2022-07-11 13:42:18 +0530

    Update API.js

--- src/Utils/API.js
+++ src/Utils/API.js
@@ -1,10 +1,5 @@
 export const TOKEN = [
-  'd0ee027c13aefab382eb0604fbf81ba9',
-  '09fd6190282bc6006ed245542372fa1e',
-  '58c12a2ed8ad8d9af53289113d492402',
-  'cf10a511dfd7abffa762e8aa313fc4bf',
-  '8ac29382462e0b8462ad1dab96e98041',
-  'bc34eed99bcde6e6b522540c086c100a'
+  'd9055439a44b99535e2a4ab62532a834'
 ];
 
 export const HEADLILNES = `https://gnews.io/api/v4/top-headlines?lang=en`;
